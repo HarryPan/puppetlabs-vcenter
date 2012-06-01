82768d3f14426e8739c94ffe5d4a7ca2ce93d192
HarryPan <harry@puppetlabs.com>
2012-06-01 16:15:31 -0700

    Check for non-existing ComputeResource.

--- lib/puppet/provider/vc_vm/default.rb
+++ lib/puppet/provider/vc_vm/default.rb
@@ -21,6 +21,12 @@ Puppet::Type.type(:vc_vm).provide(:vc_vm) do
         @root_folder,
         parse_path(parent_path + slash),
         "Invalid path for ComputeResource #{@resource[:compute_resource]}").find_child_by_name(cr_name)
+
+      if (not @immediate_parent.is_datacenter?) or compute_resource == nil:
+        raise Puppet::Modules::VCenter::ProviderBase::PathNotFoundError.new(
+          "Invalid path for VM #{@dcpath + @vmname}", __LINE__, __FILE__)
+      end
+
       @immediate_parent.real_container.vmFolder.CreateVM_Task(
                 :config => vm_spec,
                 :pool => compute_resource.resourcePool).wait_for_completion
